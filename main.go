a9ab1214d981f66e372521325ac308d8b610446e
haidongNg <nguyenhaidong1412@gmail.com>
2023-11-25 10:38:54 +0700

    Embedded our templates and added support for parsing embedded templates

--- main.go
+++ main.go
@@ -3,38 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/haidongNg/lenslocked/controllers"
+	"github.com/haidongNg/lenslocked/templates"
 	"github.com/haidongNg/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
-	tpl, err := views.Parse(filepath.Join("templates", "home.html"))
 
-	if err != nil {
-		panic(err)
-	}
-
-	r.Get("/", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "contact.html"))
-
-	if err != nil {
-		panic(err)
-	}
-
-	r.Get("/contact", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "faq.html"))
-
-	if err != nil {
-		panic(err)
-	}
-
-	r.Get("/faq", controllers.StaticHandler(tpl))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.html"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.html"))))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.html"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
