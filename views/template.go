fa8df01ea41c659fef9947ae231dc19200e2e3f6
haidongNg <nguyenhaidong1412@gmail.com>
2023-12-23 12:00:15 +0700

    Add session

--- views/template.go
+++ views/template.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/haidongNg/lenslocked/context"
+	"github.com/haidongNg/lenslocked/models"
 )
 
 func Must(t Template, err error) Template {
@@ -21,8 +23,13 @@ func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
 	tpl := template.New(pattern[0])
 	tpl = tpl.Funcs(
 		template.FuncMap{
-			"csrfField": func() template.HTML {
-				return `<input type="hidden">`
+			"csrfField": func() (template.HTML, error) {
+				return "", fmt.Errorf("csrfField not implemented")
+
+			},
+			"currentUser": func() (*models.User, error) {
+				return nil, fmt.Errorf("currentUser not implemented")
+
 			},
 		},
 	)
@@ -57,6 +64,10 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				return context.User(r.Context())
+
 			},
 		},
 	)
